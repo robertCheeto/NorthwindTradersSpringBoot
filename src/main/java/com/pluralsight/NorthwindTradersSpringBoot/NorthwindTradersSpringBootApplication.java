4828e1a173fd49ae79284006f2652f5ba4c2c746
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-12-08 14:23:56 -0500

    * added content to home for a home screen, switch case for both methods, and case for closing the program

--- src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
+++ src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 public class NorthwindTradersSpringBootApplication {
 
     private static ProductDAO productDAO;
+    private static boolean isRunning = true;
 
 	public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
@@ -18,18 +19,40 @@ public class NorthwindTradersSpringBootApplication {
 
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.println("Select an option from the choices below.");
-        System.out.println("1) List all products");
-        //System.out.println("2) Add new product");
-        System.out.print("Enter choice here: ");
-        int input = keyboard.nextInt();
-        keyboard.nextLine();
+        while (isRunning) {
+            System.out.println("Select an option from the choices below.");
+            System.out.println("1) List all products");
+            System.out.println("2) Add new product");
+            System.out.println("3) Close program");
+            System.out.print("Enter choice here: ");
+            int input = keyboard.nextInt();
+            keyboard.nextLine();
 
-        switch (input) {
-            case 1 -> productDAO.getAll().stream().forEach(System.out::println);
-            //case 2 -> productDAO.add(Product product);
-            default -> System.out.println("bad input");
+            switch (input) {
+                case 1 -> productDAO.getAll().stream().forEach(System.out::println);
+                case 2 -> {
+                    System.out.print("Enter product id: ");
+                    int productID = keyboard.nextInt();
+                    keyboard.nextLine();
+                    System.out.print("\nEnter product name: ");
+                    String productName = keyboard.nextLine();
+                    System.out.print("\nEnter product category: ");
+                    String productCategory = keyboard.nextLine();
+                    System.out.print("\nEnter product price: $");
+                    double productPrice = keyboard.nextDouble();
+                    keyboard.nextLine();
+                    productDAO.add(new Product(productID, productName, productCategory, productPrice));
+                }
+                case 3 -> {
+                    System.out.println("closing program");
+                    isRunning = false;
+                    keyboard.close();
+                    System.exit(0);
+                }
+                default -> System.out.println("bad input");
+            }
         }
+
 	}
 
 }
